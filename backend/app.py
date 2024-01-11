72a470549ee0809f8699b899519667ca3eb14481
Efe Beydogan <efebeydogan@hotmail.com>
2024-01-11 17:58:42 +0100

    separated frontend and backend folders, started backend

---
+++ backend/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+    def json(self):
+        return {'id': self.id,'username': self.username, 'role': self.role}
+
+db.create_all()
+
+@app.route('/test', methods=['GET'])
+def test():
+    return make_response(jsonify({'message': 'test route'}), 200)
+
+# create a user
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    try:
+        data = request.get_json()
+        new_user = User(username=data['username'], role=data['role'])
+        db.session.add(new_user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'user created'}), 201)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error creating user'}), 500)
+
+# get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    try:
+        users = User.query.all()
+        return make_response(jsonify([user.json() for user in users]), 200)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting users'}), 500)
+
+# get a user by id
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            return make_response(jsonify({'user': user.json()}), 200)
+        return make_response(jsonify({'message': 'user not found'}), 404)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error getting user'}), 500)
