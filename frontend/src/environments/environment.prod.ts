d6b529eabe84671b8842dbfa36e670f9401d1bc2
lduits <l.b.m.duits@student.vu.nl>
2024-01-22 22:22:10 +0100

    Creation branch to test network policies

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // API_BASE_URL: 'https://api.quiz-app.com'
-    API_BASE_URL: 'https://api.quiz-app-helm.com'
+    API_BASE_URL: 'https://api.quiz-app.com'
+    // API_BASE_URL: 'https://api.quiz-app-helm.com'
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // API_BASE_URL: 'https://api.quiz-app.com'
-    API_BASE_URL: 'https://api.quiz-app-helm.com' // the helm url
+    API_BASE_URL: 'https://api.quiz-app.com'
+    // API_BASE_URL: 'https://api.quiz-app-helm.com' // the helm url
 }
